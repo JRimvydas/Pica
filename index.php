aa3f5393d3b06226e5b0ccb9667cb1bdcc7dc947
JohnWick1975 <vitalijus.rusakas@gmail.com>
2020-05-29 11:01:31 +0300

    add style css and grid arrays changed

--- index.php
+++ index.php
@@ -16,13 +16,8 @@ $nav = [
 ];
 
 $grid = [
-    'grid' => [
-        'https://cdn.fishki.net/upload/post/2018/01/17/2485804/tn/19-1.jpg',
-        'https://cdn.fishki.net/upload/post/201406/06/1275305/catsmob_daily_picdump_04040_047.jpg',
-        'https://www.austinchronicle.com/imager/b/original/1651196/bba8/sid.jpg',
-        'https://s.zefirka.net/images/2018-06-28/krasivye-devushki-lyubyat-piccu/krasivye-devushki-lyubyat-piccu-19.jpg',
-        'https://s.erofishki.cc/upload/post/2019/03/04/2898245/tn/1376.jpg',
-        'https://imageproxy.ifunny.co/noop/user_photos/dbaf18a31d42c311e6007aec645fa7c22fb53b3d_0.jpg%3F1484760104'
+    'class' => [
+        'pica-' => rand(1, 6),
     ],
 ];
 
@@ -35,6 +30,7 @@ $grid = [
               content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>Document</title>
+        <link rel="stylesheet" type="text/css" href="assets/css/style.css">
     </head>
     <body>
         <main>
