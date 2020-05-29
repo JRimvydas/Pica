984bbc5fd39dba86d97f383122ed05d3665a3f2f
Pijus <user@users-MacBook-Pro.local>
2020-05-29 14:51:00 +0300

    home page finished

--- index.php
+++ index.php
@@ -2,39 +2,67 @@
 
 $nav = [
     'nav' => [
-        'index' => [
+        'Index' => [
             'link' => 'index.php',
         ],
-        'about' => [
+        'About' => [
             'link' => 'about.php',
         ],
-        'form' => [
+        'Form' => [
             'link' => 'form.php',
         ]
     ]
 
 ];
 
-$grid = [
-    'class' => [
-        'pica-' => rand(1, 6),
-    ],
-];
+$class = array_keys($nav)[0];
+
+
+/**
+ * generate_matrix function generates a grid
+ *
+ * @param  int $size
+ * @return array
+ */
+function generate_matrix(int $size): array
+{
+    $matrix = [];
+    for ($i = 0; $i < $size; $i++) {
+        for ($j = 0; $j < $size; $j++) {
+            $matrix[$i][$j] = rand(0, 1);
+        }
+    }
+
+    return $matrix;
+}
+
+$number = rand(3, 6);
+$matrix = generate_matrix($number);
 
 ?>
-<!doctype html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport"
-              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Document</title>
-        <link rel="stylesheet" type="text/css" href="assets/css/style.css">
-    </head>
-    <body>
-        <main>
 
-        </main>
-    </body>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+</head>
+
+<body>
+    <?php require 'navbar.php'; ?>
+    <main>
+        <?php foreach ($matrix as $key => $row) : ?>
+            <div>
+                <?php foreach ($row as $square) : ?>
+                    <div class="grid grid-<?php print $number; ?> <?php print $square; ?> pica-<?php print rand(1, 6); ?> "></div>
+                <?php endforeach; ?>
+            </div>
+        <?php endforeach; ?>
+        <div class="title">
+            <h1>PZ2A PICERIJA</h1>
+        </div>
+    </main>
+</body>
+
 </html>
