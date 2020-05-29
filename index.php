9f9e4034327a96be86b32a499f7e86e0b8b69c8d
JohnWick1975 <vitalijus.rusakas@gmail.com>
2020-05-29 10:34:51 +0300

    created nav&grid arrays

--- index.php
+++ index.php
@@ -1,5 +1,31 @@
 <?php
 
+$nav = [
+    'nav' => [
+        'index' => [
+            'link' => 'index.php',
+        ],
+        'about' => [
+            'link' => 'about.php',
+        ],
+        'form' => [
+            'link' => 'form.php',
+        ]
+    ]
+
+];
+
+$grid = [
+    'grid' => [
+        'https://cdn.fishki.net/upload/post/2018/01/17/2485804/tn/19-1.jpg',
+        'https://cdn.fishki.net/upload/post/201406/06/1275305/catsmob_daily_picdump_04040_047.jpg',
+        'https://www.austinchronicle.com/imager/b/original/1651196/bba8/sid.jpg',
+        'https://s.zefirka.net/images/2018-06-28/krasivye-devushki-lyubyat-piccu/krasivye-devushki-lyubyat-piccu-19.jpg',
+        'https://s.erofishki.cc/upload/post/2019/03/04/2898245/tn/1376.jpg',
+        'https://imageproxy.ifunny.co/noop/user_photos/dbaf18a31d42c311e6007aec645fa7c22fb53b3d_0.jpg%3F1484760104'
+    ],
+];
+
 ?>
 <!doctype html>
 <html lang="en">
